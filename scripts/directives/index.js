1a46b816945152ee87e4124c20cd397a94e5d082
indooorsman <indooorsman@gmail.com>
2015-09-18 15:39:07 +0800

    新建一个指令来处理出错的图片

---
+++ scripts/directives/index.js
@@ -0,0 +1,20 @@
+angular.module('timelineApp.directives', [])
+    .directive('cardCover', ['$http', function ($http) {
+      return {
+        restrict: 'A',
+        link: function (scope, ele, attr) {
+          var img = attr.cardCover;
+          $http.head(img).then(function success() {
+            ele.css('background-image', 'url(' + img + ')');
+          }, function error() {
+            ele.css({
+              'height': 'auto',
+              'text-align': 'center',
+              'background-color': '#cccccc',
+              'padding': '20px 0'
+            });
+            ele.html('图片加载出错 :(')
+          });
+        }
+      }
+    }]);
--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,14 @@
 angular.module('timelineApp', [
   'ngMaterial',
   'timelineApp.controllers',
-  'timelineApp.services'
+  'timelineApp.services',
+  'timelineApp.directives'
 ]).config(['$mdThemingProvider', function ($mdThemingProvider) {
   $mdThemingProvider.theme('default');
-      //.primaryPalette('blue')
-      //.accentPalette('light-blue')
-      //.warnPalette('orange')
-      //.backgroundPalette('red');
+  //.primaryPalette('blue')
+  //.accentPalette('light-blue')
+  //.warnPalette('orange')
+  //.backgroundPalette('red');
 }]);
 
 angular.element(document).ready(function () {
